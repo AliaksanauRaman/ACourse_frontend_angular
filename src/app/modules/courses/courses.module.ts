e71585d8fa21bdb3611ff069c5c4037f32dd61c0
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-07 18:47:11 +0200

    Minor clean up

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesIndexPageComponent } from './pages/courses-index-page/courses-index-page.component';
 import { CoursesHttpService } from './services/courses-http.service';
 import { COURES_HTTP_API_URL } from './tokens/courses-http-api-url.token';
 import { COURSES_INDEX_PAGE_HTTP_SERVICE } from './tokens/courses-index-page-http-service.token';
-import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
