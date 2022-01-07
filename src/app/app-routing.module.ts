a145384ec55f6482a508051d40e57f55142a65fc
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-07 20:36:46 +0200

    Add LoginPageComponent to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LoginPageComponent } from './modules/core/pages/login-page/login-page.component';
+
 const appRoutes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     redirectTo: '/courses',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
   },
   {
     path: 'courses',
