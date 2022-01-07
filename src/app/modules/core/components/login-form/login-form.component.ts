00efc07d6e5ff5b644bbd8c7e89c128110b1ff8e
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-07 20:26:42 +0200

    Create LoginFormComponent component

---
+++ src/app/modules/core/components/login-form/login-form.component.ts
@@ -0,0 +1,59 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { map, takeUntil, tap } from "rxjs";
+
+import { AutoDestroy } from "../../../../shared/classes/auto-destroy.class";
+
+export type LoginFormValue = Readonly<{
+  email: string;
+  password: string;
+}>;
+
+@Component({
+  selector: 'ac-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginFormComponent extends AutoDestroy implements OnInit {
+  @Output()
+  public readonly valueChange = new EventEmitter<LoginFormValue>();
+
+  @Output()
+  public readonly validityChange = new EventEmitter<boolean>();
+
+  public loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(70)]],
+    password: ['', [Validators.required, Validators.maxLength(50)]],
+  });
+
+  constructor(
+    private readonly formBuilder: FormBuilder,
+  ) {
+    super();
+  }
+
+  public ngOnInit(): void {
+    this.subToLoginFormValueChanges();
+    this.subToLoginFormValidityChanges();
+  }
+
+  private subToLoginFormValueChanges(): void {
+    this.loginForm.valueChanges
+      .pipe(
+        tap(formValue => this.valueChange.emit(formValue)),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+
+  private subToLoginFormValidityChanges(): void {
+    this.loginForm.statusChanges
+      .pipe(
+        map((loginFormStatus) => loginFormStatus === 'VALID'),
+        tap(formIsValid => this.validityChange.emit(formIsValid)),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+}
