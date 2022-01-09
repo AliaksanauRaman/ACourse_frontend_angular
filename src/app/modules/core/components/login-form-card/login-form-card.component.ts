c3d2698a99a7c095641164bfe3447e42d0739a33
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-09 09:27:37 +0100

    Add login form card component

---
+++ src/app/modules/core/components/login-form-card/login-form-card.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: 'ac-login-form-card',
+  templateUrl: './login-form-card.component.html',
+  styleUrls: ['./login-form-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginFormCardComponent {
+
+}
