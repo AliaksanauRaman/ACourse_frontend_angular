368eabc3fe112056501a544b6ae1ef08c6fa9d02
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-12 11:44:29 +0100

    Move all markup, styles and logic related to login form card to the corresponding component

--- src/app/modules/core/components/login-form-card/login-form-card.component.ts
+++ src/app/modules/core/components/login-form-card/login-form-card.component.ts
@@ -1,4 +1,10 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { catchError, of, tap } from "rxjs";
+import { LocalStorageKey, LocalStorageService } from "../../../../shared/services/local-storage.service";
+
+import { LoginFormValue } from "../../types/login-form-value.type";
+
+import { AuthenticationService } from "../../services/authentication.service";
 
 @Component({
   selector: 'ac-login-form-card',
@@ -7,5 +13,35 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginFormCardComponent {
+  public loginFormValue: LoginFormValue = {
+    email: '',
+    password: '',
+  };
+  public loginFormValid = false;
 
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+    private readonly localStorageService: LocalStorageService,
+  ) {}
+
+  public handleLoginFormValueChange(value: LoginFormValue): void {
+    this.loginFormValue = value;
+  }
+
+  public handleLoginFormValidityChange(valid: boolean): void {
+    this.loginFormValid = valid;
+  }
+
+  public handleSubmitClick(): void {
+    this.authenticationService
+      .loginUser(this.loginFormValue)
+      .pipe(
+        tap(({ accessToken }) => this.localStorageService.save(LocalStorageKey.ACCESS_TOKEN, accessToken)),
+        catchError(error => {
+          console.log({ error });
+          return of(error);
+        }),
+      )
+      .subscribe();
+  }
 }
