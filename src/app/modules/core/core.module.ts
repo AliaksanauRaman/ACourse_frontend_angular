68c65bd0e3bcea7ace53b2efe89ff2d1684e83de
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-12 11:53:07 +0100

    Add all needed declarations, imports and providers to CoreModule

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
@@ -7,10 +8,13 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { LoginFormCardComponent } from "./components/login-form-card/login-form-card.component";
+import { AuthenticationService } from "./services/authentication.service";
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, MatInputModule, MatCardModule, MatButtonModule],
-  declarations: [LoginPageComponent, LoginFormComponent],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, MatInputModule, MatCardModule, MatButtonModule],
+  declarations: [LoginPageComponent, LoginFormComponent, LoginFormCardComponent],
   exports: [LoginPageComponent],
+  providers: [AuthenticationService],
 })
 export class CoreModule {}
