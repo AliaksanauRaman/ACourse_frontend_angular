c6e693c85edcc0e6eb6836a2d94d81934bcdffd6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 10:40:06 +0100

    Declare IndexPageComponent

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
+import { IndexPageComponent } from "./pages/index-page/index-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
 import { LoginFormCardComponent } from "./components/login-form-card/login-form-card.component";
@@ -23,8 +24,16 @@ import { SnackBarModule } from "../../shared/modules/snack-bar";
     MatButtonModule,
     MatProgressSpinnerModule,
   ],
-  declarations: [LoginPageComponent, LoginFormComponent, LoginFormCardComponent],
-  exports: [LoginPageComponent],
+  declarations: [
+    IndexPageComponent,
+    LoginPageComponent,
+    LoginFormComponent,
+    LoginFormCardComponent,
+  ],
+  exports: [
+    IndexPageComponent,
+    LoginPageComponent,
+  ],
   providers: [AuthenticationService],
 })
 export class CoreModule {}
