80089fdfe10a17054b84aee9425c27665131e508
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 10:07:20 +0100

    Add SnackBarModule and MatProgressSpinnerModule to CoreModule, remove HttpClientModule from there

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,18 +1,28 @@
 import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { LoginFormComponent } from "./components/login-form/login-form.component";
 import { LoginFormCardComponent } from "./components/login-form-card/login-form-card.component";
 import { AuthenticationService } from "./services/authentication.service";
 
+import { SnackBarModule } from "../../shared/modules/snack-bar";
+
 @NgModule({
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, MatInputModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SnackBarModule.forRoot(),
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   declarations: [LoginPageComponent, LoginFormComponent, LoginFormCardComponent],
   exports: [LoginPageComponent],
   providers: [AuthenticationService],
