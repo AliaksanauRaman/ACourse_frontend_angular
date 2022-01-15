0ab8e7a693a8a93b75a70f0057a33e8e62b5f209
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 09:58:27 +0100

    Update AuthenticationService

--- src/app/modules/core/services/authentication.service.ts
+++ src/app/modules/core/services/authentication.service.ts
@@ -1,15 +1,15 @@
 import { Inject, Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import jwtDecode from "jwt-decode";
 
-import { API_URL } from "../../../tokens/api-url.token";
 import { UserCredentials } from "../types/user-credentials.type";
-import { LoginUserResponse } from "../types/login-user-response.type";
+import { SuccessUserLoginResponse } from "../types/success-user-login-response.type";
+import { isDecodedJwtToken } from "../types/decoded-jwt-token.type";
 
-// TODO: Move to some shared place
-const defaultHeaders = new HttpHeaders({
-  'Content-Type': 'application/json',
-});
+import { API_URL } from "../../../shared/injection-tokens/api-url";
+import { LocalStorageService, LocalStorageKey } from "../../../shared/services/local-storage.service";
+import { JSON_HTTP_HEADERS } from "../../../shared/constants/json-http-headers";
 
 @Injectable()
 export class AuthenticationService {
@@ -17,15 +17,44 @@ export class AuthenticationService {
     @Inject(API_URL)
     private readonly apiUrl: string,
     private readonly httpClient: HttpClient,
+    private readonly localStorageService: LocalStorageService,
   ) {}
 
-  public loginUser(userCredentials: UserCredentials): Observable<LoginUserResponse> {
-    return this.httpClient.post<LoginUserResponse>(
+  public makeHttpRequestToLoginUserByCredentials(
+    userCredentials: UserCredentials,
+  ): Observable<SuccessUserLoginResponse> {
+    return this.httpClient.post<SuccessUserLoginResponse>(
       `${this.apiUrl}/api/auth/login`,
       userCredentials,
       {
-        headers: defaultHeaders,
+        headers: JSON_HTTP_HEADERS,
       },
     );
+  }
+
+  public isUserLoggedIn(): boolean {
+    const jwtToken = this.getJwtTokenOfCurrentUserOrNull();
+
+    if (jwtToken === null) {
+      return false;
+    }
+
+    return this.isJwtTokenValid(jwtToken);
+  }
+
+  private getJwtTokenOfCurrentUserOrNull(): string | null {
+    return this.localStorageService.get(LocalStorageKey.ACCESS_TOKEN);
+  }
+
+  // TODO: Think about differentiation between session expired and
+  // invalid decoded token
+  private isJwtTokenValid(token: string): boolean {
+    const decodedJwtToken = jwtDecode<unknown>(token);
+
+    if (!isDecodedJwtToken(decodedJwtToken)) {
+      return false;
+    }
+
+    return (decodedJwtToken.exp * 1000) >= new Date().getTime();
   }
 }
