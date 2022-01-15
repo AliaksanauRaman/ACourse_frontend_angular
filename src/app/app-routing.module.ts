9c3b77369619a4d6a71e4406db8c8b1c5a217a0c
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 09:59:23 +0100

    Use IsUserNotAuthenticatedGuard and IsUserAuthenticatedGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { IsUserAuthenticatedGuard } from './modules/core/guards/is-user-authenticated.guard';
+import { IsUserNotAuthenticatedGuard } from './modules/core/guards/is-user-not-authenticated.guard';
+
 import { LoginPageComponent } from './modules/core/pages/login-page/login-page.component';
 
 const appRoutes: Routes = [
@@ -11,10 +14,12 @@ const appRoutes: Routes = [
   },
   {
     path: 'login',
+    canActivate: [IsUserNotAuthenticatedGuard],
     component: LoginPageComponent,
   },
   {
     path: 'courses',
+    canActivate: [IsUserAuthenticatedGuard],
     loadChildren: () => import('./modules/courses/courses.module').then(m => m.CoursesModule),
   },
 ];
