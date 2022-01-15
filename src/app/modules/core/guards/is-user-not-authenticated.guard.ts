6d9c72aa69083a8bcdd8439f73faed196bf14bc4
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 09:58:42 +0100

    Add IsUserNotAuthenticatedGuard guard

---
+++ src/app/modules/core/guards/is-user-not-authenticated.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from "@angular/router";
+
+import { AuthenticationService } from "../services/authentication.service";
+
+@Injectable()
+export class IsUserNotAuthenticatedGuard implements CanActivate {
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+  ) {}
+
+  public async canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Promise<boolean> {
+    return !this.authenticationService.isUserLoggedIn();
+  }
+}
