940245bbe29ea03e458374765d88cbd253dbaee3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 09:00:10 +0100

    Add HttpRequestsInterceptor

---
+++ src/app/modules/core/interceptors/http-requests.interceptor.ts
@@ -0,0 +1,35 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { API_URL } from "../../../tokens/api-url.token";
+
+import { LocalStorageService, LocalStorageKey } from "../../../shared/services/local-storage.service";
+
+@Injectable()
+export class HttpRequestsInterceptor implements HttpInterceptor {
+  constructor(
+    @Inject(API_URL) private readonly apiUrl: string,
+    private readonly localStorageService: LocalStorageService,
+  ) {}
+
+  public intercept(
+    httpRequest: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(
+      httpRequest.url.includes(this.apiUrl)
+        ? this.getHttpRequestWithAuthorizationHeaders(httpRequest)
+        : httpRequest,
+    );
+  }
+
+  private getHttpRequestWithAuthorizationHeaders(httpRequest: HttpRequest<unknown>): HttpRequest<unknown> {
+    const accessToken = this.localStorageService.get(LocalStorageKey.ACCESS_TOKEN);
+    return httpRequest.clone({
+      setHeaders: accessToken !== null
+        ? { Authorization: `Bearer ${accessToken}` }
+        : {},
+    });
+  }
+}
