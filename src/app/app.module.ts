9c9551197d2e530b785e8b22b278e4a1cdb326c3
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 10:12:03 +0100

    Add app interceptors to the app module, update path to API_URL injection token, add import of HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { CoreModule } from './modules/core/core.module';
+import { HttpRequestsInterceptor } from './modules/core/interceptors/http-requests.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { API_URL } from './tokens/api-url.token';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { API_URL } from "./shared/injection-tokens/api-url";
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     CoreModule,
     AppRoutingModule,
   ],
@@ -22,6 +26,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     {
       provide: API_URL,
       useValue: 'https://acourse-backend.herokuapp.com'
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestsInterceptor,
+      multi: true,
     },
   ],
   bootstrap: [AppComponent]
