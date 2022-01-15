af6501cd2273fa23c54121fbca2403907af297da
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 10:38:25 +0100

    Add showInfo method to SnackBarService

--- src/app/shared/modules/snack-bar/services/snack-bar.service.ts
+++ src/app/shared/modules/snack-bar/services/snack-bar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
 
+import { InfoSnackBarComponent } from "../components/info-snack-bar/info-snack-bar.component";
 import { ErrorSnackBarComponent } from "../components/error-snack-bar/error-snack-bar.component";
 
 const DEFAULT_DURATION_IN_MS = 2000;
@@ -10,6 +11,16 @@ export class SnackBarService {
   constructor(
     private readonly snackBar: MatSnackBar,
   ) {}
+
+  public showInfo(message: string, duration = DEFAULT_DURATION_IN_MS): void {
+    this.snackBar.openFromComponent(
+      InfoSnackBarComponent,
+      {
+        data: { message },
+        duration,
+      },
+    );
+  }
 
   public showError(message: string, duration = DEFAULT_DURATION_IN_MS): void {
     this.snackBar.openFromComponent(
