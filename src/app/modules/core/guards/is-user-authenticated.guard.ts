92108f4218685311dad95b59b7456e0915397d52
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-15 09:58:56 +0100

    Add IsUserAuthenticatedGuard guard

---
+++ src/app/modules/core/guards/is-user-authenticated.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+
+import { SnackBarService } from "../../../shared/modules/snack-bar";
+
+import { AuthenticationService } from "../services/authentication.service";
+
+@Injectable()
+export class IsUserAuthenticatedGuard implements CanActivate {
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+    private readonly snackBarService: SnackBarService,
+    private readonly router: Router,
+  ) {}
+
+  public async canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Promise<boolean> {
+    if (this.authenticationService.isUserLoggedIn()) {
+      return true;
+    }
+
+    this.snackBarService.showError('Your session has expired!');
+
+    return this.router.navigate(['/login']).then(() => false);
+  }
+}
