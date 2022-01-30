e0f0d5b57c1f1029cd35c082fd4607aed723114a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-30 12:20:45 +0100

    Create CourseCardComponent

---
+++ src/app/modules/courses/components/course-card/course-card.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+
+import { Course } from '../../types/course.type';
+
+@Component({
+  selector: 'ac-course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  public course!: Course;
+
+  public ngOnInit(): void {
+    if (this.course === undefined) {
+      throw new Error("Course is not defined!");
+    }
+  }
+}
