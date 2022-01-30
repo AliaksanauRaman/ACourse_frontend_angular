b7e619a33b422efb365dc7359780d3af9936f60a
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-30 12:19:14 +0100

    Create SettingsModule

---
+++ src/app/modules/settings/settings.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { SettingsRoutingModule } from './settings-routing.module';
+
+@NgModule({
+  imports: [
+    SettingsRoutingModule
+  ],
+  exports: [],
+  providers: [],
+})
+export class SettingsModule { }
