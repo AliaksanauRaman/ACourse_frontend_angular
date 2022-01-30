75cd629eb27e2e2f97833db3035eeefc436251e8
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-01-30 12:21:15 +0100

    Add CourseCardComponent to CoursesModule declarations and MatCardModule to CoursesModule imports

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesIndexPageComponent } from './pages/courses-index-page/courses-index-page.component';
 import { CoursesHttpService } from './services/courses-http.service';
 import { COURSES_INDEX_PAGE_HTTP_SERVICE } from './tokens/courses-index-page-http-service.token';
+import { CourseCardComponent } from './components/course-card/course-card.component';
 
 @NgModule({
   declarations: [
-    CoursesIndexPageComponent
+    CourseCardComponent,
+    CoursesIndexPageComponent,
   ],
   imports: [
     CommonModule,
+    MatCardModule,
     CoursesRoutingModule,
   ],
   providers: [
