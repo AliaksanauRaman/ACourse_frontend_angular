b02f17101928082a9fd204e0e91d9c2aa98717f6
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-03-08 20:19:50 +0100

    Enhance prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { AcEnvironment } from "../app/shared/types/environment.type";
+
+export const environment: AcEnvironment = {
+  production: true,
+  apiUrl: "https://acourse-backend.herokuapp.com",
 };
