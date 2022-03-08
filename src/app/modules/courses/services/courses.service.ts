6e9a2d6175a99cf18e620dbac9ff794d1fc372c1
Raman Aliaksanau <raman.aliaksanau@gmail.com>
2022-03-08 20:00:10 +0100

    Make initial implementation of CoursesService

---
+++ src/app/modules/courses/services/courses.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+
+import { AddCourseDialogComponent } from "../components/add-course-dialog/add-course-dialog.component";
+import { ICoursesService } from "../interfaces/courses-service.interface";
+
+@Injectable()
+export class CoursesService implements ICoursesService {
+  constructor(
+    private readonly matDialog: MatDialog,
+  ) {}
+
+  public openAddCourseDialog(): void {
+    const dialogRef = this.matDialog.open(AddCourseDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
+}
